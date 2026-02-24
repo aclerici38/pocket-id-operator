96c58f094220019b2185c0cbaf19b0e1f8a165f7
Anthony Clerici <anthony@clerici.me>
2026-02-23 19:23:58 -0800

    chore: track latest tested pocket-id version

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-openapi/strfmt v0.25.0
 	github.com/onsi/ginkgo/v2 v2.28.1
 	github.com/onsi/gomega v1.39.1
+	golang.org/x/mod v0.32.0
 	k8s.io/api v0.35.1
 	k8s.io/apimachinery v0.35.1
 	k8s.io/client-go v0.35.1
@@ -89,7 +90,6 @@ require (
 	go.yaml.in/yaml/v2 v2.4.3 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
-	golang.org/x/mod v0.32.0 // indirect
 	golang.org/x/net v0.49.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
---
+++ internal/controller/instance/version.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package instance
+
+import (
+	"regexp"
+	"strings"
+
+	"golang.org/x/mod/semver"
+)
+
+const (
+	// latestTestedPocketIDVersion is the most recent pocket-id upstream version tested.
+	// renovate: datasource=docker depName=ghcr.io/pocket-id/pocket-id
+	latestTestedPocketIDVersion = "v2.2.0"
+)
+
+var semverTagRe = regexp.MustCompile(`^v\d+\.\d+\.\d+`)
+
+// pocketIDVersionStatus extracts the semver tag from an image string and reports
+// whether it is newer than latestTestedPocketIDVersion.
+// Returns the extracted version string (empty if unparseable) and whether it is newer.
+func pocketIDVersionStatus(image string) (vStr string, newer bool) {
+	tag := image
+	if i := strings.Index(tag, "@"); i != -1 {
+		tag = tag[:i]
+	}
+	if i := strings.LastIndex(tag, ":"); i != -1 {
+		tag = tag[i+1:]
+	} else {
+		return "", false
+	}
+
+	vStr = semverTagRe.FindString(tag)
+	if vStr == "" || !semver.IsValid(vStr) {
+		return "", false
+	}
+
+	return vStr, semver.Compare(vStr, latestTestedPocketIDVersion) > 0
+}
