b875ba9b4d1e75d964eb707c36023809607b535b
Anthony Clerici <anthony@clerici.me>
2026-02-23 19:45:34 -0800

    fix: programatically get versions in version test

--- internal/controller/instance/version_test.go
+++ internal/controller/instance/version_test.go
@@ -16,9 +16,28 @@ limitations under the License.
 
 package instance
 
-import "testing"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+
+	"golang.org/x/mod/semver"
+)
+
+// minorBump returns a version string with the minor component incremented by delta.
+// e.g. minorBump("v2.2.0", 1) -> "v2.3.0", minorBump("v2.2.0", -1) -> "v2.1.0"
+func minorBump(v string, delta int) string {
+	parts := strings.Split(strings.TrimPrefix(semver.Canonical(v), "v"), ".")
+	minor, _ := strconv.Atoi(parts[1])
+	return fmt.Sprintf("v%s.%d.0", parts[0], minor+delta)
+}
 
 func TestPocketIDVersionStatus(t *testing.T) {
+	current := latestTestedPocketIDVersion
+	older := minorBump(current, -1)
+	newer := minorBump(current, 1)
+
 	tests := []struct {
 		name        string
 		image       string
@@ -27,26 +46,26 @@ func TestPocketIDVersionStatus(t *testing.T) {
 	}{
 		{
 			name:        "current tested version with distroless suffix",
-			image:       "ghcr.io/pocket-id/pocket-id:v2.2.0-distroless@sha256:abc123",
-			wantVersion: "v2.2.0",
+			image:       "ghcr.io/pocket-id/pocket-id:" + current + "-distroless@sha256:abc123",
+			wantVersion: current,
 			wantNewer:   false,
 		},
 		{
 			name:        "current tested version plain",
-			image:       "ghcr.io/pocket-id/pocket-id:v2.2.0",
-			wantVersion: "v2.2.0",
+			image:       "ghcr.io/pocket-id/pocket-id:" + current,
+			wantVersion: current,
 			wantNewer:   false,
 		},
 		{
 			name:        "older version",
-			image:       "ghcr.io/pocket-id/pocket-id:v2.1.0-distroless",
-			wantVersion: "v2.1.0",
+			image:       "ghcr.io/pocket-id/pocket-id:" + older + "-distroless",
+			wantVersion: older,
 			wantNewer:   false,
 		},
 		{
 			name:        "newer version triggers warning",
-			image:       "ghcr.io/pocket-id/pocket-id:v2.3.0-distroless",
-			wantVersion: "v2.3.0",
+			image:       "ghcr.io/pocket-id/pocket-id:" + newer + "-distroless",
+			wantVersion: newer,
 			wantNewer:   true,
 		},
 		{
