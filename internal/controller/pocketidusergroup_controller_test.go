551920f5c7fc70726e474fc727b025e6eeab79dc
Anthony Clerici <anthony@clerici.me>
2026-03-02 00:02:42 -0800

    chore: migrate all types from envValue to sensitiveValue (#145)
    * chore: migrate all types from envValue to sensitiveValue
    
    * chore: regenerate manifests
    
    * workaround gofmt changing the CEL quotes
    
    ---------
    
    Co-authored-by: botthebuilder25[bot] <200541989+botthebuilder25[bot]@users.noreply.github.com>

--- internal/controller/pocketidusergroup_controller_test.go
+++ internal/controller/pocketidusergroup_controller_test.go
@@ -112,7 +112,7 @@ var _ = Describe("PocketIDUserGroup Controller", func() {
 					Namespace: namespace,
 				},
 				Spec: pocketidinternalv1alpha1.PocketIDInstanceSpec{
-					EncryptionKey: pocketidinternalv1alpha1.EnvValue{Value: "0123456789abcdef"},
+					EncryptionKey: pocketidinternalv1alpha1.SensitiveValue{Value: "0123456789abcdef"},
 				},
 			}
 			Expect(k8sClient.Create(ctx, instance)).To(Succeed())
@@ -172,7 +172,7 @@ var _ = Describe("PocketIDUserGroup Controller", func() {
 					Namespace: namespace,
 				},
 				Spec: pocketidinternalv1alpha1.PocketIDInstanceSpec{
-					EncryptionKey: pocketidinternalv1alpha1.EnvValue{Value: "0123456789abcdef"},
+					EncryptionKey: pocketidinternalv1alpha1.SensitiveValue{Value: "0123456789abcdef"},
 				},
 			}
 			Expect(k8sClient.Create(ctx, instance)).To(Succeed())
@@ -182,7 +182,7 @@ var _ = Describe("PocketIDUserGroup Controller", func() {
 					Namespace: namespace,
 				},
 				Spec: pocketidinternalv1alpha1.PocketIDInstanceSpec{
-					EncryptionKey: pocketidinternalv1alpha1.EnvValue{Value: "fedcba9876543210"},
+					EncryptionKey: pocketidinternalv1alpha1.SensitiveValue{Value: "fedcba9876543210"},
 				},
 			}
 			Expect(k8sClient.Create(ctx, instance)).To(Succeed())
